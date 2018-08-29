676928edc813f57fe0a9ea98c85d1c7d79a02ac2
Shawn Hopkins <shopkins@seagen.com>
2018-08-29 12:59:50 -0700

    add user guide

--- trackapp/urls.py
+++ trackapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 
         path('', views.CS096ListView.as_view(), name='entry_list'),
         path('add/', views.CS096CreateView.as_view(), name='entry_add'),
-        path('user/<int:pk>/', views.AddUserTest.as_view(), name='user_add'),
+        path('user/<int:pk>/', views.AddUserView.as_view(), name='user_add'),
         path('details/<int:pk>/',views.CS096UpdateView.as_view(), name='entry_update'),
         path('ajax/load-protocols/', views.load_protocols, name='ajax_load_protocols'),
         path('delete/<int:pk>/',views.CS096DeleteView.as_view(), name='entry_delete'),
--- trackapp/views.py
+++ trackapp/views.py
@@ -1,35 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django import forms
-from django.forms import modelformset_factory
 from django.shortcuts import render,redirect,reverse
 from trackapp.models import *
-from django.utils import timezone
 from django import  http
 from django.views.generic.edit import FormView,View
-from django_tables2 import RequestConfig
-from .tables import CS096Table
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from .serializers import ProductSerializer,ProtocolSerializer
-
 from trackapp.forms import CS096Form,UserAddForm
-
-
 from django.views.generic import ListView, CreateView, UpdateView,DetailView,DeleteView
 from django.urls import reverse_lazy
 from .models import Protocol,Product,CS096
 
-# Display a list of records
+
 class CS096ListView(ListView):
+    """ List view for parent table CS096"""
+
     model = CS096
     template_name = 'list.html'
     queryset=CS096.objects.all().order_by('project')
     context_object_name = 'entries'
 
-
-# Add a new recordd and display the list
 class CS096CreateView(CreateView):
+    """ Model form to handle Create for CS096 """
+
     model = CS096
     queryset = CS096.objects.all()
     form_class = CS096Form
@@ -39,25 +30,27 @@ class CS096CreateView(CreateView):
     def form_invalid(self, form):
         return http.HttpResponse(form.fields)
 
-class CS096CreateUserView(CreateView):
-    model = UserAccess
-
-    form_class = UserAddForm
-    template_name = 'adduser.html'
-    success_url = reverse_lazy('entry_list')
-
-    def form_invalid(self, form):
-        return http.HttpResponse(form.fields)
-
-class AddUserTest(View):
+class AddUserView(View):
+    """ View to manage Create for child table UserAccess """
 
     def get(self,request,pk):
+        """
+        Handle GET request returns empty UserAccess form with reference to parent CS096 object
+        :param request:
+        :param pk: Foreign key for CS096
+        :return: UserAccess form
+        """
         req=CS096.objects.get(pk=pk)
         form=UserAddForm()
-
         return render(request, 'adduser.html', {'form': form,'cs096_':req})
 
     def post(self,request,pk):
+        """
+        Handle POST request - creates new UserAccess record related to CS096 by PK
+        :param request:
+        :param pk: FK to CS096
+        :return: redirect to list
+        """
         form=UserAddForm(request.POST)
         if form.is_valid():
 
@@ -66,9 +59,8 @@ class AddUserTest(View):
             return redirect('/')
 
 
-
-# Update existing record and return list
 class CS096UpdateView(UpdateView):
+    """ Model view updates CS096 records """
     model = CS096
     template_name = 'details.html'
     form_class = CS096Form
@@ -76,53 +68,72 @@ class CS096UpdateView(UpdateView):
     success_url = reverse_lazy('entry_list')
 
 class CS096UpdateUserView(UpdateView):
+    """ Model view updates UserAccess record """
     model = UserAccess
     template_name = 'user_details.html'
     form_class = UserAddForm
     queryset =  UserAccess.objects.filter()
     success_url = reverse_lazy('entry_list')
 
-# AJAX insert dependent protocols based on selected  product
 def load_protocols(request):
+    """ Insert child protocol for selected product into cascading select elements"""
     id = request.GET.get('protocols')
     protocols=Protocol.objects.filter(product_id=id)
     return render(request, 'dropdown.html', {'protocols': protocols})
 
-# Delete a record and return list
 class CS096DeleteView(DeleteView):
+    """ Handle POST to delete a CS096 record"""
     template_name = 'delete.html'
     form_class = CS096Form
     queryset =  CS096.objects.filter()
     success_url = reverse_lazy('entry_list')
 
     def get(self, *a, **kw):
+        """
+        Delete a CS096 record
+        :param a: WSGI GET request for CS096 record
+        :param kw: PK - primary key for CS096 record
+        :return:
+        """
         return self.delete(*a, **kw)
 
-# Delete a single user
 class CS096DeleteUserView(DeleteView):
+    """ Delete a single user from UserAccess"""
     template_name = 'delete_user.html'
     queryset =  UserAccess.objects.filter()
     success_url = reverse_lazy('entry_list')
 
     def get(self, *a, **kw):
+        """
+        Delete a UserAcess record
+        :param a: WSGI GET request for UserAccess record
+        :param kw: PK - primary key for UserAccess record
+        :return:
+        """
         return self.delete(*a, **kw)
-
-
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    Endpoint that returns all Products
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProtocolViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    Enpoint that returns protocols by product
     """
     queryset = Protocol
     serializer_class = ProtocolSerializer
+
+
+
+
+
+
+
+
 
     # def main_view(request):
     #     if request.method=="POST":
@@ -135,3 +146,12 @@ class ProtocolViewSet(viewsets.ModelViewSet):
     #     else:
     #         form=CS096Form()
     #         return render(request,'index.html',{'form':form})
+    # class CS096CreateUserView(CreateView):
+    #     model = UserAccess
+    #
+    #     form_class = UserAddForm
+    #     template_name = 'adduser.html'
+    #     success_url = reverse_lazy('entry_list')
+    #
+    #     def form_invalid(self, form):
+    #         return http.HttpResponse(form.fields)
